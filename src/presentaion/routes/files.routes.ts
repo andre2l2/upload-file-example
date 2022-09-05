0fa1e6f0e3cdb4f6fdceef2b773cb6a7f9e3dd65
Andre Ferreira <delcferreira@gmail.com>
2022-09-04 21:41:50 -0300

    fix: remove onError

--- src/presentaion/routes/files.routes.ts
+++ src/presentaion/routes/files.routes.ts
@@ -2,9 +2,12 @@ import { Router } from 'express';
 
 const filesRoutes = Router().post('/file', (req, res) => {
   const body = req.body;
+
   console.log(body);
 
-  res.json({ value: 'ok' }).status(201);
+  // fs.writeFileSync(`test.png`, body.buffer);
+
+  res.json({ value: 'ok' }).status(200);
 });
 
 export { filesRoutes };
